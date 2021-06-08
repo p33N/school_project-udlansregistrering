e7a68a21066bb9e85d057a3399e63fc3ddbffe32
Nick <41940951+Invusius@users.noreply.github.com>
2021-06-08 15:26:15 +0200

    post ikke done

--- produkter.php
+++ produkter.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 <html>
     <head>
@@ -42,39 +41,89 @@
                             success: function(result){
                                 result.forEach(element => {
                                     $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
-                                    $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
-                                    $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
-                                    $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
+                        
                                     });
                                 }
                             }); 
                         </script>
                 </table>
 
-                <form action="functions/edit_form.php" id="productform">
+                <form action="functions/edit_form.php" id="dsafdsggdsproductform">
                     <label for="id">ID:</label><br>
-                    <input type="text" id="id" name="id" value="John"><br>
+                    <input type="text" id="id" name="id" ><br>
                     <label for="maerke">Mærke:</label><br>
-                    <input type="text" id="maerke" name="maerke" value="John"><br>
+                    <input type="text" id="maerke" name="maerke" ><br>
                     <label for="model">Model:</label><br>
-                    <input type="text" id="model" name="model" value="John"><br>
+                    <input type="text" id="model" name="model" ><br>
                     <label for="status">Status:</label><br>
-                    <input type="text" id="status" name="status" value="John"><br>
+                    <input type="text" id="status" name="status" ><br>
                     <label for="laaner">Låner:</label><br>
-                    <input type="text" id="laaner" name="laaner" value="John"><br>
+                    <input type="text" id="laaner" name="laaner" ><br>
                     <input type="submit" id="btn-ret" value="Ændre">
                     <input type="submit" id="btn-slet" value="Slet">
                 </form> 
 
-                <form action="functions/create_form.php" id="productform">
+                <form id="productform">
                     <label for="id">ID:</label><br>
-                    <input type="text" id="id" name="id" value="John"><br>
+                    <input type="text" id="id" name="ComputerId" ><br>
                     <label for="maerke">Mærke:</label><br>
-                    <input type="text" id="maerke" name="maerke" value="John"><br>
+                    <input type="text" id="maerke" name="Brand" ><br>
                     <label for="model">Model:</label><br>
-                    <input type="text" id="model" name="model" value="John"><br>
-                    <input type="submit" id="btn-opret" value="oPr3T">
+                    <input type="text" id="model" name="Model" ><br>
+                    <input type="hidden" name="StatusID" value="0">
+                    <button type="" id="tester" >tester</button>
+                    
+                    
+                    <!--    <input type="" id="btn-opret" value="oPr3T">     -->
+                
+                
                 </form> 
+                
+                <script>
+                 $.fn.serializeObject = function() {
+                    var o = {};
+                    var a = this.serializeArray();
+                    $.each(a, function() {
+                        if (o[this.name]) {
+                            if (!o[this.name].push) {
+                                o[this.name] = [o[this.name]];
+                            }
+                            o[this.name].push(this.value || '');
+                        } else {
+                            o[this.name] = this.value || '';
+                        }
+                    });
+                    return o;
+                };
+                        $("#tester").on("click",function(){
+                         
+                            var myData = $("#productform").serializeObject(); 
+                            //alert(JSON.stringify(myData)); 
+                            //var test = JSON.stringify(myData); 
+
+                            $.ajax({
+                            type: "POST",
+                            url:"http://10.130.16.147:57414/api/computer",
+                            headers: {
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            },
+                            data:  {Data: JSON.stringify(myData)} ,
+                            contentType: "application/json; charset=utf-8",
+                            dataType: "json",
+                            success: function(result){
+                                
+                            },
+                            error: function (errMsg) {
+                                console.log(errMsg); 
+                                
+                            }
+                            
+                            }); 
+                    
+                        }); 
+
+                </script>
+
 
             </div>
             <div class="footer">
