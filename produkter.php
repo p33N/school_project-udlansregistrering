7550823f4630761ea483247d850ddaf7f7b2d17c
Niklas <41940807+p33N@users.noreply.github.com>
2021-06-09 09:35:36 +0200

    v0.5
    Version with working create function and styling done on the list. (Still missing the scroll function on the list)

--- produkter.php
+++ produkter.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 <html>
     <head>
@@ -34,6 +33,7 @@
                     </tr>
                         <!-- API call --> 
                         <script> 
+                        
                             $.ajax({
                             url:"http://10.130.16.147:57414/api/computer",
                             type: "GET",
@@ -42,39 +42,83 @@
                             success: function(result){
                                 result.forEach(element => {
                                     $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
-                                    $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
-                                    $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
-                                    $("#productstable").append("<tr> <td> " + element.computerId + " </td> </tr> ");
+                        
                                     });
                                 }
-                            }); 
+                            });
+                            
                         </script>
                 </table>
 
-                <form action="functions/edit_form.php" id="productform">
+                <form  id="productform-edit">
                     <label for="id">ID:</label><br>
-                    <input type="text" id="id" name="id" value="John"><br>
+                    <input type="text" id="id" name="id" ><br>
                     <label for="maerke">Mærke:</label><br>
-                    <input type="text" id="maerke" name="maerke" value="John"><br>
+                    <input type="text" id="maerke" name="maerke" ><br>
                     <label for="model">Model:</label><br>
-                    <input type="text" id="model" name="model" value="John"><br>
+                    <input type="text" id="model" name="model" ><br>
                     <label for="status">Status:</label><br>
-                    <input type="text" id="status" name="status" value="John"><br>
+                    <input type="text" id="status" name="status" ><br>
                     <label for="laaner">Låner:</label><br>
-                    <input type="text" id="laaner" name="laaner" value="John"><br>
+                    <input type="text" id="laaner" name="laaner" ><br>
                     <input type="submit" id="btn-ret" value="Ændre">
                     <input type="submit" id="btn-slet" value="Slet">
                 </form> 
 
-                <form action="functions/create_form.php" id="productform">
+                <form id="productform">
                     <label for="id">ID:</label><br>
-                    <input type="text" id="id" name="id" value="John"><br>
+                    <input type="text" id="id" name="ComputerId" ><br>
                     <label for="maerke">Mærke:</label><br>
-                    <input type="text" id="maerke" name="maerke" value="John"><br>
+                    <input type="text" id="maerke" name="Brand" ><br>
                     <label for="model">Model:</label><br>
-                    <input type="text" id="model" name="model" value="John"><br>
-                    <input type="submit" id="btn-opret" value="oPr3T">
+                    <input type="text" id="model" name="Model" ><br>
+                    <input type="hidden" name="StatusID" value="0">
+                    <button id="create" >Opret</button>             
                 </form> 
+                
+                <script>
+                 $.fn.serializeObject = function() {
+                    var o = {};
+                    var a = this.serializeArray();
+                    $.each(a, function() {
+                        if (o[this.name]) {
+                            if (!o[this.name].push) {
+                                o[this.name] = [o[this.name]];
+                            }
+                            o[this.name].push(this.value || '');
+                        } else {
+                            o[this.name] = this.value || '';
+                        }
+                    });
+                    return o;
+                };
+                        $("#create").click(function(){
+                            event.preventDefault();
+                            var myData = $("#productform").serializeObject(); 
+                            $(this).closest('form').find("input[type=text], textarea").val("");
+                            $.ajax({
+                            type: "POST",
+                            url:"http://10.130.16.147:57414/api/computer",
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            data:  JSON.stringify(myData) ,
+                            contentType: "application/json",
+                            dataType: "json",
+                            success: function(result){
+                                alert("pc was inserted")
+                            },
+                            error: function (errMsg) {
+                                console.log(errMsg); 
+                                
+                            }
+                            
+                            }); 
+                    
+                        }); 
+
+                </script>
+
 
             </div>
             <div class="footer">
